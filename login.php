e135d8d3e231b00a528a424faa7cd973e6da4ff4
ArcySanta <arcysanta@gmail.com>
2023-10-01 18:38:58 +0200

    upload
    index, login, style

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Book your travel - Logowanie</title>
+
+    <meta name="description" content="Travel website">
+	<meta name="keywords" content="travel, booking, map">
+	<meta name="author" content="Patryk Ruzicki, Łukasz Stępień">
+	
+	<link href="style.css" rel="stylesheet">
+</head>
+<body id="top">
+    <header>
+        <div id="logo">
+            <img src="logo.png" alt="Logo Book your travel">
+        </div>
+        <nav>
+			<ol>
+				<li><a href="index.html">Strona główna</a></li>
+				<li><a id="current" href="#">Mój profil</a></li>
+				<li><a href="register.php">Rejestracja</a></li>
+			</ol>
+		</nav>
+    </header>
+    <main>
+        <section>
+            <form action="">
+                Login <input type="text" name="login" id="login">
+                Hasło <input type="text" name="password" id="password">
+            </form>
+        </section>
+    </main>
+    <footer>
+        <p>Stronę stworzono w 2023r.</p><p><a href="#top">Do poczatku strony</a></p>
+    </footer>
+</body>
+</html>
