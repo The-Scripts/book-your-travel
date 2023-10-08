1a5b95bad75bb115c70548086d6dd88195a20bcb
Łukasz Stępień <lukaszatepien@gmail.com>
2023-10-08 16:12:34 +0200

    Create classes to refactor Markers.php

---
+++ php/DatabaseConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace db;
+class DatabaseConfig
+{
+    protected static $servername = "localhost";
+    protected static $username = "root";
+    protected static $password = "";
+    protected static $database = "db";
+}
---
+++ php/DatabaseConn.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace db;
+
+class DatabaseConn extends DatabaseConfig
+{
+    public function __construct()
+    {
+
+    }
+}
