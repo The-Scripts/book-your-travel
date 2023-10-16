c3dc09cc2c8aee26fe2de32aa296417f010bb0a2
Patryk-Ruzicki <arcysanta@gmail.com>
2023-10-16 09:51:17 +0100

    register

--- php/register.php
+++ php/register.php
@@ -1,8 +1,36 @@
 <?php
+/*
     require_once "db/DatabaseConn.php";
 
     $conn = new db\DatabaseConn();
+*/
 
-    $login = trim($_POST['login']);
-    $pass = trim($_POST['password']);
+    // Connect to the database
+    $mysqli = new mysqli("localhost", "root", "", "db");
+    
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+    
+    // Get user input
+    $email = $_POST["login"];
+    $password = $_POST["password"];
+    
+    // Hash the password
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+    
+    // Insert user data into the database
+    $sql = "INSERT INTO users (Email, Password) VALUES (?, ?)";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("ss", $email, $hashed_password);
+    
+    if ($stmt->execute()) {
+        echo "Registration successful!";
+    } else {
+        echo "Registration failed: " . $stmt->error;
+    }
+    
+    // Close the database connection
+    $mysqli->close();
+    ?>
     
