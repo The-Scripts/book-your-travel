f39d08be7718116eaf4ef2b5758e71a2f11c751d
Łukasz Stępień <lukaszatepien@gmail.com>
2023-10-24 22:17:04 +0200

    Markers: Checking date

--- src/js/map/initMap.js
+++ src/js/map/initMap.js
@@ -22,60 +22,63 @@ async function initMap() {
 
     let markers = [];
     for (let i = 0; i < markersData.length; i++) {
-        const marker = document.createElement('div');
-        marker.className = 'marker';
-        marker.innerHTML = `${markersData[i].Title} <span class="price">${markersData[i].Price} PLN</span><br>
+        if (new Date < new Date(markersData[i].StartDate)) {
+            const marker = document.createElement('div');
+            marker.className = 'marker';
+            marker.innerHTML = `${markersData[i].Title} <span class="price">${markersData[i].Price} PLN</span><br>
                             ${markersData[i].StartDate} → ${markersData[i].EndDate}`;
-        markers[i] = new AdvancedMarkerElement({
-            position: { lat: parseFloat(markersData[i].Latitude), lng: parseFloat(markersData[i].Longitude) },
-            map: map,
-            content: marker,
-            title: `${i+1}`
-        });
+            markers[i] = new AdvancedMarkerElement({
+                position: { lat: parseFloat(markersData[i].Latitude), lng: parseFloat(markersData[i].Longitude) },
+                map: map,
+                content: marker,
+                title: `${i+1}`
+            });
 
-        markers[i].addListener('click', async evt => {
-            fetch('../../../php/popUp.php', {
-                method: 'POST',
-                body: JSON.stringify({id: `${i+1}`}),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(response => response.json())
-                .then(responseJson => {
-                    sessionStorage.setItem('id', `${i+1}`)
-                    sessionStorage.setItem('title', `${responseJson[0]['Title']}`)
-                    sessionStorage.setItem('date', responseJson[0]['StartDate'] + ' → ' + responseJson[0]['EndDate'])
-                    sessionStorage.setItem('description', `${responseJson[0]['Description']}`)
-                    sessionStorage.setItem('price', responseJson[0]['Price'] + " PLN")
-
-                    popupTitle.textContent = responseJson[0]['Title'];
-                    popupDate.textContent = responseJson[0]['StartDate'] + ' → ' + responseJson[0]['EndDate'];
-                    popupDescription.textContent = responseJson[0]['Description'];
-                    popupPrice.textContent = responseJson[0]['Price'] + " PLN";
+            markers[i].addListener('click', async evt => {
+                fetch('../../../php/popUp.php', {
+                    method: 'POST',
+                    body: JSON.stringify({id: `${i+1}`}),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
                 })
+                    .then(response => response.json())
+                    .then(responseJson => {
+                        sessionStorage.setItem('id', `${i+1}`)
+                        sessionStorage.setItem('title', `${responseJson[0]['Title']}`)
+                        sessionStorage.setItem('date', responseJson[0]['StartDate'] + ' → ' + responseJson[0]['EndDate'])
+                        sessionStorage.setItem('description', `${responseJson[0]['Description']}`)
+                        sessionStorage.setItem('price', responseJson[0]['Price'] + " PLN")
 
-            fetch('../../../php/popUpImage.php', {
-                method: 'POST',
-                body:JSON.stringify({id: `${i+1}`}),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(response => response.json())
-                .then(responseJson => {
-                    sessionStorage.setItem('imageSrc', `${responseJson[0]['Image']}`);
-                    popupImg.setAttribute('src', `${responseJson[0]['Image']}`);
+                        popupTitle.textContent = responseJson[0]['Title'];
+                        popupDate.textContent = responseJson[0]['StartDate'] + ' → ' + responseJson[0]['EndDate'];
+                        popupDescription.textContent = responseJson[0]['Description'];
+                        popupPrice.textContent = responseJson[0]['Price'] + " PLN";
+                    })
+
+                fetch('../../../php/popUpImage.php', {
+                    method: 'POST',
+                    body:JSON.stringify({id: `${i+1}`}),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
                 })
+                    .then(response => response.json())
+                    .then(responseJson => {
+                        sessionStorage.setItem('imageSrc', `${responseJson[0]['Image']}`);
+                        popupImg.setAttribute('src', `${responseJson[0]['Image']}`);
+                    })
 
-            popupSection.classList.remove('hide');
-            markers.forEach((el) => el.zIndex = 0);
-            map.setZoom(11);
-            map.setCenter(markers[i].position)
-            markers[i].zIndex = 1;
+                popupSection.classList.remove('hide');
+                markers.forEach((el) => el.zIndex = 0);
+                map.setZoom(11);
+                map.setCenter(markers[i].position)
+                markers[i].zIndex = 1;
 
 
 
-        })
+            })
+        }
+
     }
  }
