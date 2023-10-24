168c79e0917b6a7278bed3923d832040645a17b5
ArcySanta <arcysanta@gmail.com>
2023-10-24 22:11:20 +0200

    alerts/errors

--- php/login.php
+++ php/login.php
@@ -1,43 +1,77 @@
 <?php
 session_start();
 $mysqli = new mysqli("localhost", "root", "", "db");
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Book your travel - Error</title>
 
-if ($mysqli->connect_error) {
-    die("Błąd połączenia z bazą danych " . $mysqli->connect_error);
-}
+    <meta name="description" content="Travel website">
+	<meta name="keywords" content="travel, booking, map">
+	<meta name="author" content="Patryk Ruzicki, Łukasz Stępień">
+	
+	<link href="../src/css/style.css" rel="stylesheet">
+</head>
+<body id="top">
+    <header>
+        <div id="logo">
+            <a href="../index.html"><img src="../res/img/logo.png" alt="Logo Book your travel"></a>
+        </div>
+        <nav>
+			<ol>
+				<li><a href="../index.html">Strona główna</a></li>
+				<li><a href="../src/html/login.php">Mój profil</a></li>
+				<li><a href="../src/html/register.php">Rejestracja</a></li>
+			</ol>
+		</nav>
+    </header>
+    <main>
+    <?php
+        if ($mysqli->connect_error) {
+            die("<div class='error'>Błąd połączenia z bazą danych " . $mysqli->connect_error . "</div>");
+        }
 
-@$email = $_POST["login"];
-@$password = $_POST["password"];
+        @$email = $_POST["login"];
+        @$password = $_POST["password"];
 
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    //header('Location: ../src/html/login.html');
-    die("Niepoprawny adres email.");
-}
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            //header('Location: ../src/html/login.html');
+            die("<div class='error'>Niepoprawny adres email.</div>");
+        }
 
-$retrieve_password_query = "SELECT ID, Email, Password FROM users WHERE Email = ?";
-$stmt = $mysqli->prepare($retrieve_password_query);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$stmt->store_result();
+        $retrieve_password_query = "SELECT ID, Email, Password FROM users WHERE Email = ?";
+        $stmt = $mysqli->prepare($retrieve_password_query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
 
-if ($stmt->num_rows == 1) {
-    $stmt->bind_result($user_id, $user_email, $hashed_password);
-    $stmt->fetch();
+        if ($stmt->num_rows == 1) {
+            $stmt->bind_result($user_id, $user_email, $hashed_password);
+            $stmt->fetch();
 
-    if (password_verify($password, $hashed_password)) {
-        $_SESSION["user_id"] = $user_id;
-        $_SESSION["user_email"] = $user_email;
+            if (password_verify($password, $hashed_password)) {
+                $_SESSION["user_id"] = $user_id;
+                $_SESSION["user_email"] = $user_email;
 
-        header("Location: dashboard.php");
-    } else {
-        //header('Location: ../src/html/login.html');
-        die("Niepoprawne hasło.");
-    }
-} else {
-    //header('Location: ../src/html/login.html');
-    die("Nie znaleziono emaila. Proszę zarejestruj się, jeżeli nie masz stworzonego konta.");
-}
+                header("Location: dashboard.php");
+            } else {
+                //header('Location: ../src/html/login.html');
+                die("<div class='error'>Niepoprawne hasło.</div>");
+            }
+        } else {
+            //header('Location: ../src/html/login.html');
+            die("<div class='error'>Nie znaleziono emaila. Proszę zarejestruj się, jeżeli nie masz stworzonego konta.</div>");
+        }
+    ?>
+    </main>
+    <footer>
+        <p>Stronę stworzono w 2023r.</p><p><a href="#top">Do poczatku strony</a></p>
+    </footer>
+</body>
+</html>
 
-// Close the database connection
+<?php
 $mysqli->close();
 ?>
--- php/register.php
+++ php/register.php
@@ -1,66 +1,92 @@
 <?php
-// Connect to the database
+session_start();
 $mysqli = new mysqli("localhost", "root", "", "db");
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Book your travel - Error</title>
 
-if ($mysqli->connect_error) {
-    die("Błąd połączenia z bazą danych " . $mysqli->connect_error);
-}
+    <meta name="description" content="Travel website">
+	<meta name="keywords" content="travel, booking, map">
+	<meta name="author" content="Patryk Ruzicki, Łukasz Stępień">
+	
+	<link href="../src/css/style.css" rel="stylesheet">
+</head>
+<body id="top">
+    <header>
+        <div id="logo">
+            <a href="../index.html"><img src="../res/img/logo.png" alt="Logo Book your travel"></a>
+        </div>
+        <nav>
+			<ol>
+				<li><a href="../index.html">Strona główna</a></li>
+				<li><a href="../src/html/login.php">Mój profil</a></li>
+				<li><a href="../src/html/register.php">Rejestracja</a></li>
+			</ol>
+		</nav>
+    </header>
+    <main>
+    <?php
+        if ($mysqli->connect_error) {
+            die("<div class='error'>Błąd połączenia z bazą danych " . $mysqli->connect_error . "</div>");
+        }
 
-// Get user input
-$email = $_POST["login"];
-$password = $_POST["password"];
-$repeat_password = $_POST["check_pass"];
+        $email = $_POST["login"];
+        $password = $_POST["password"];
+        $repeat_password = $_POST["check_pass"];
 
-// Check if the email is valid
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    die("Nieprawidłowy adres email.");
-}
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            die("<div class='error'>Nieprawidłowy adres email.</div>");
+        }
 
-// Check if the email already exists in the database
-$check_email_query = "SELECT users.Email FROM users;";
-$equal = $mysqli -> query($check_email_query);
-$num_of_email = $equal -> num_rows;
-$email_count = 0;
+        $check_email_query = "SELECT users.Email FROM users;";
+        $equal = $mysqli -> query($check_email_query);
+        $num_of_email = $equal -> num_rows;
+        $email_count = 0;
 
-for($i = 0; $i < $num_of_email; $i++){
-    $row = $equal -> fetch_assoc();
-    if($row['Email'] == $email)
-        $email_count++;
-}
+        for($i = 0; $i < $num_of_email; $i++){
+            $row = $equal -> fetch_assoc();
+            if($row['Email'] == $email)
+                $email_count++;
+        }
 
-if ($email_count > 0) {
-    die("Podany email już istnieje. Proszę użyj innego maila.");
-}
+        if ($email_count > 0) {
+            die("<div class='error'>Podany email już istnieje. Proszę użyj innego maila.</div>");
+        }
 
-// Check if the password and repeated password match
-if ($password !== $repeat_password) {
-    die("Hasła nie pasują do siebie");
-}
+        if ($password !== $repeat_password) {
+            die("<div class='error'>Hasła nie pasują do siebie</div>");
+        }
 
-// Check if the password meets certain criteria (e.g., at least 8 characters, including numbers, uppercase, and lowercase letters)
-if (strlen($password) < 8 || !preg_match("/[0-9]/", $password) || !preg_match("/[a-z]/", $password) || !preg_match("/[A-Z]/", $password)) {
-    die("Hasło musi spełniać następujące kryteria:<br>
-        - Co najmniej 8 znaków długości<br>
-        - Musi zawierać przynajmniej jedną cyfrę (0-9)<br>
-        - Musi zawierać przynajmniej jedną małą literę (a-z)<br>
-        - Musi zawierać przynajmniej jedną wielką literę (A-Z)");
-}
+        if (strlen($password) < 8 || !preg_match("/[0-9]/", $password) || !preg_match("/[a-z]/", $password) || !preg_match("/[A-Z]/", $password)) {
+            die("<div class='error'>Hasło musi spełniać następujące kryteria:<br><ol>
+                <li>Co najmniej 8 znaków długości</li>
+                <li>Musi zawierać przynajmniej jedną cyfrę (0-9)</li>
+                <li>Musi zawierać przynajmniej jedną małą literę (a-z)</li>
+                <li>Musi zawierać przynajmniej jedną wielką literę (A-Z)</li></ol></div>");
+        }
 
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 
-// Hash the password
-$hashed_password = password_hash($password, PASSWORD_BCRYPT);
+        $sql = "INSERT INTO users (Email, Password) VALUES (?, ?)";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("ss", $email, $hashed_password);
 
-// Insert user data into the database
-$sql = "INSERT INTO users (Email, Password) VALUES (?, ?)";
-$stmt = $mysqli->prepare($sql);
-$stmt->bind_param("ss", $email, $hashed_password);
+        if ($stmt->execute()) {
+            echo "<div class='corre'>Rejestracja udana!</div>";
+        } else {
+            echo "<div class='error'>Rejestracja nie powiodła się: " . $stmt->error . "</div>";
+        }
+    ?>
+    </main>
+    <footer>
+        <p>Stronę stworzono w 2023r.</p><p><a href="#top">Do poczatku strony</a></p>
+    </footer>
+</body>
+</html>
 
-if ($stmt->execute()) {
-    echo "Rejestracja udana!";
-} else {
-    echo "Rejestracja nie powiodła się: " . $stmt->error;
-}
-
-// Close the database connection
+<?php
 $mysqli->close();
 ?>
