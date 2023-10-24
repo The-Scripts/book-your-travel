36060c30e1f4de20abc348f0854e5a969627eb94
ArcySanta <arcysanta@gmail.com>
2023-10-24 23:02:36 +0200

    better data in db
    test data:
    - username: user@example.com
    - password: Qwerty12!

--- res/sql/db.sql
+++ res/sql/db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Oct 14, 2023 at 06:32 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1
+-- Czas generowania: 24 Paź 2023, 22:59
+-- Wersja serwera: 10.4.22-MariaDB
+-- Wersja PHP: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,173 +18,174 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db`
+-- Baza danych: `db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookedtravels`
+-- Struktura tabeli dla tabeli `bookedtravels`
 --
 
 CREATE TABLE `bookedtravels` (
-                                 `ID` int(11) NOT NULL,
-                                 `UserID` int(11) NOT NULL,
-                                 `OfferID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `OfferID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `bookedtravels`
+-- Zrzut danych tabeli `bookedtravels`
 --
 
 INSERT INTO `bookedtravels` (`ID`, `UserID`, `OfferID`) VALUES
-    (1, 1, 2);
+(4, 1, 1),
+(5, 1, 3),
+(6, 1, 2),
+(7, 1, 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `imgs`
+-- Struktura tabeli dla tabeli `imgs`
 --
 
 CREATE TABLE `imgs` (
-                        `ID` int(11) NOT NULL,
-                        `Image` varchar(255) NOT NULL,
-                        `OfferID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID` int(11) NOT NULL,
+  `Image` varchar(255) NOT NULL,
+  `OfferID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `imgs`
+-- Zrzut danych tabeli `imgs`
 --
 
 INSERT INTO `imgs` (`ID`, `Image`, `OfferID`) VALUES
-                                                  (1, 'https://i.ibb.co/QKmLBCr/Adobe-Stock-317823565.jpg', 1),
-                                                  (2, 'https://i.ibb.co/QKmLBCr/Adobe-Stock-317823565.jpg', 2),
-                                                  (3, 'https://i.ibb.co/M5G5q9s/Adobe-Stock-324433656.jpg', 3),
-                                                  (4, 'https://i.ibb.co/r3ztjK0/Adobe-Stock-404194596.jpg', 4);
+(1, 'https://imgs.search.brave.com/KCkwEQ2qhv3ZhJrvxx5tWnIvsHdOYRrLwDUoxtzExhE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS1jZG4udHJpcGFk/dmlzb3IuY29tL21l/ZGlhL3Bob3RvLW8v/MDgvYTQvODcvODgv/dHJhZm8tYmFzZS1j/YW1wLmpwZw', 1),
+(2, 'https://imgs.search.brave.com/UqSN6eVgQhpIh6lZSUn4vitTKTqTBbMzOwRreNcbp4s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/bm9jbGVnaS1vbmxp/bmUucGwvcG9rb2pl/LWdvc2Npbm5lLXct/cmF0dXN6dS1vZ3Jv/ZHppZW5pZWMtMzU4/NDNtLmpwZw', 2),
+(3, 'https://imgs.search.brave.com/l_pr9rmqB70ug_0IQIbv8b4WxhxhF2Z7xF-kSzzax8U/rs:fit:860:0:0/g:ce/aHR0cDovL3d3dy5n/b3NjaW5pZWNvcmxp/a3dtaXJvd2llLnBs/L2ltZy9nYWxlcmlh/X2thdC9vcmxpa19i/dWR5bmVrLmpwZw', 3),
+(4, 'https://imgs.search.brave.com/rBtD3EOPOHSnL-zWO7CC7cbBkYPRcjfOKnP3vNJIUPU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZXRl/b3ItdHVyeXN0eWth/LnBsL2ltYWdlcy9i/YXNlLzUvNDIxMi80/ODgzMTFfNDAuanBn', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `offers`
+-- Struktura tabeli dla tabeli `offers`
 --
 
 CREATE TABLE `offers` (
-                          `ID` int(11) NOT NULL,
-                          `Title` varchar(255) NOT NULL,
-                          `Description` text DEFAULT NULL,
-                          `Price` decimal(10,2) NOT NULL,
-                          `StartDate` date NOT NULL,
-                          `EndDate` date NOT NULL,
-                          `Latitude` decimal(21,16) NOT NULL,
-                          `Longitude` decimal(21,16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID` int(11) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Description` text DEFAULT NULL,
+  `Price` decimal(10,2) NOT NULL,
+  `StartDate` date NOT NULL,
+  `EndDate` date NOT NULL,
+  `Latitude` decimal(21,16) NOT NULL,
+  `Longitude` decimal(21,16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `offers`
+-- Zrzut danych tabeli `offers`
 --
 
 INSERT INTO `offers` (`ID`, `Title`, `Description`, `Price`, `StartDate`, `EndDate`, `Latitude`, `Longitude`) VALUES
-                                                                                                                  (1, 'Trafo', 'Przygoda w górach', 500.00, '2023-07-01', '2023-07-15', 50.5665674351534660, 19.5356050517405680),
-                                                                                                                  (2, 'Ratusz Ogr', 'Przygoda w górach', 800.00, '2024-08-10', '2024-08-25', 50.4532462119902050, 19.5238280825227730),
-                                                                                                                  (3, 'Orlik Mirów', 'Przygoda w górach', 300.00, '2024-09-05', '2024-09-12', 50.6150298790832740, 19.4725325729212170),
-                                                                                                                  (4, 'Łutowiec', 'Przygoda w górach', 500.00, '2024-10-06', '2024-10-09', 50.6281700505497500, 19.4525109219685750);
+(1, 'Trafo Base Camp', '<p>\r\nNiesamowita przygoda z noclegiem w najlepszym możliwym miejscu w Podlesicach. Trafo Base Camp oferuje wyśmienitą kuchnię salę treningową i konferencyjną ognisko i ogródek. Jest też osobno sklep z sprzętem turystycznym i wspinaczkowym.\r\n</p>\r\n<p>\r\nW cenie wycieczki poza wspaniałym noclegiem i pożywieniem jest również przyszykowana wyprawa w skały z doświadczonymi instruktorami, którzy przeprowadzą pełęn kurs.\r\n</p>', '2450.00', '2023-07-01', '2023-07-15', '50.5665674351534660', '19.5356050517405680'),
+(2, 'Ratusz Ogrodzieniec', '<p>\r\nNiesamowita przygoda czeka na Ciebie w Ratuszu w Ogrodzieńcu. Nasz urokliwy obiekt oferuje nie tylko komfortowe noclegi, ale także wiele innych atrakcji. Nasza restauracja serwuje wyśmienite dania, które zadowolą nawet najbardziej wybrednych smakoszy.\r\n</p>\r\n<p>\r\nJednak to nie wszystko! Nasz ogródek to idealne miejsce na relaks i odpoczynek na świeżym powietrzu, a wieczorem możesz cieszyć się atmosferą ogniska.\r\n</p>\r\n<p>\r\nW cenie pobytu oferujemy także przewodzoną wyprawę po okolicznych skałach z doświadczonymi przewodnikami. Zaplanowana jest równierz wizyta w pobliskim zamku. Czekamy na Ciebie, byś mógł przeżyć niezapomniane chwile w naszym Ratuszu.\r\n</p>', '4800.00', '2024-08-10', '2024-08-25', '50.4532462119902050', '19.5238280825227730'),
+(3, 'Orlik Mirów', '<p>\r\nPrzyjedź do Orlika Mirów i doświadcz niesamowitej przygody! Nasze urokliwe miejsce oferuje nie tylko komfortowy nocleg, ale także wiele innych atrakcji, które sprawią, że Twój pobyt będzie niezapomniany.\r\n</p>\r\n<p>\r\nNasza stołówka serwuje wyśmienite śniadania i kolacje, które zadowolą nawet najbardziej wybrednych smakoszy.\r\n</p>\r\n<p>\r\nOrlik Mirów ma także ognisko, które możesz rozpalić na wieczór by poczuć klimat i ciepło. \r\n</p>\r\n<p>\r\nAle to nie koniec! W cenie pobytu oferujemy przewodzoną wyprawę po okolicznych atrakcjach wraz z doświadczonymi instruktorami i przewodnikami, którzy przekażą Ci niesamowite historie. To wyjątkowa okazja, by poznać uroki Mirowa i odwiedzić ruiny zamku. \r\n</p>\r\n<p>\r\nZapraszamy do Orlika Mirów, byś mógł przeżyć niezapomniane chwile w naszym pięknym miejscu.\r\n</p>', '3200.00', '2024-09-05', '2024-09-12', '50.6150298790832740', '19.4725325729212170'),
+(4, 'Agroturystyka pod Strażnicą', '<p>\r\nZaplanuj swoją wyjątkową przygodę w Agroturystyce pod Strażnicą w Łutowcu! Nasza urokliwa agroturystyka oferuje nie tylko wygodne noclegi, ale również wiele innych atrakcji, które uczynią Twój pobyt niezapomnianym.\r\n</p>\r\n<p>\r\nNasza kuchnia to prawdziwa uczta dla podniebienia, serwując dania przygotowywane z produktów od miejscowych dostawców, które zadowolą najbardziej wymagających smakoszy.\r\n</p>\r\n<p>\r\nAgroturystyka pod Strażnicą to także oaza spokoju, gdzie w naszym uroczym ogrodzie możesz cieszyć się relaksem na świeżym powietrzu. Wieczorem zapraszamy do wspólnego ogniska, by podzielić się historiami i wrażeniami. Dysponujemy również wiatą pod, którą możemy znaleźć grila i stoły z łąwami dla wszystkich zmęczonych.\r\n</p>\r\n<p>\r\nJeśli jesteś miłośnikiem aktywności na świeżym powietrzu, to nasza agroturystyka jest idealnym miejscem. W okolicy znajdziesz liczne szlaki turystyczne i atrakcje.\r\n</p>\r\n<p>\r\nAle to nie koniec! W cenie pobytu oferujemy także przewodzoną wyprawę po okolicznych atrakcjach wraz z doświadczonymi przewodnikami, którzy podzielą się swoją wiedzą i pasją do regionu.\r\n</p>\r\n<p>\r\nTo wyjątkowa okazja, by poznać tę malowniczą okolicę. Serdecznie zapraszamy do Agroturystyki pod Strażnicą, byś mógł przeżyć niezapomniane chwile w naszym urokliwym miejscu.\r\n</p>', '2150.00', '2024-10-06', '2024-10-09', '50.6281700505497500', '19.4525109219685750');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktura tabeli dla tabeli `users`
 --
 
 CREATE TABLE `users` (
-                         `ID` int(11) NOT NULL,
-                         `Email` varchar(255) NOT NULL,
-                         `Password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID` int(11) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Zrzut danych tabeli `users`
 --
 
 INSERT INTO `users` (`ID`, `Email`, `Password`) VALUES
-                                                    (1, 'jan@example.com', 'haslo123'),
-                                                    (2, 'anna@example.com', 'haslo456'),
-                                                    (3, 'mateusz@example.com', 'haslo789');
+(1, 'user@example.com', '$2y$10$ozrv6dheFGJagY7WxztCQuZcY/29ZpZWDLkkAW8QUHm6zT1sD52ha');
 
 --
--- Indexes for dumped tables
+-- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `bookedtravels`
+-- Indeksy dla tabeli `bookedtravels`
 --
 ALTER TABLE `bookedtravels`
-    ADD PRIMARY KEY (`ID`),
+  ADD PRIMARY KEY (`ID`),
   ADD KEY `UserID` (`UserID`),
   ADD KEY `OfferID` (`OfferID`);
 
 --
--- Indexes for table `imgs`
+-- Indeksy dla tabeli `imgs`
 --
 ALTER TABLE `imgs`
-    ADD PRIMARY KEY (`ID`),
+  ADD PRIMARY KEY (`ID`),
   ADD KEY `imgs_offers_ID_fk` (`OfferID`);
 
 --
--- Indexes for table `offers`
+-- Indeksy dla tabeli `offers`
 --
 ALTER TABLE `offers`
-    ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `users`
+-- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
-    ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT dla zrzuconych tabel
 --
 
 --
--- AUTO_INCREMENT for table `bookedtravels`
+-- AUTO_INCREMENT dla tabeli `bookedtravels`
 --
 ALTER TABLE `bookedtravels`
-    MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `imgs`
+-- AUTO_INCREMENT dla tabeli `imgs`
 --
 ALTER TABLE `imgs`
-    MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `offers`
+-- AUTO_INCREMENT dla tabeli `offers`
 --
 ALTER TABLE `offers`
-    MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-    MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- Constraints for dumped tables
+-- Ograniczenia dla zrzutów tabel
 --
 
 --
--- Constraints for table `bookedtravels`
+-- Ograniczenia dla tabeli `bookedtravels`
 --
 ALTER TABLE `bookedtravels`
-    ADD CONSTRAINT `bookedtravels_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`ID`),
+  ADD CONSTRAINT `bookedtravels_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`ID`),
   ADD CONSTRAINT `bookedtravels_ibfk_2` FOREIGN KEY (`OfferID`) REFERENCES `offers` (`ID`);
 
 --
--- Constraints for table `imgs`
+-- Ograniczenia dla tabeli `imgs`
 --
 ALTER TABLE `imgs`
-    ADD CONSTRAINT `imgs_offers_ID_fk` FOREIGN KEY (`OfferID`) REFERENCES `offers` (`ID`);
+  ADD CONSTRAINT `imgs_offers_ID_fk` FOREIGN KEY (`OfferID`) REFERENCES `offers` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
