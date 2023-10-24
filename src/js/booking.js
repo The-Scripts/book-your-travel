9e87da2c5a5027e4aca5f227530c0ba57b857ed0
Łukasz Stępień <lukaszatepien@gmail.com>
2023-10-24 16:35:14 +0200

    Div animation and show data in confirm div

--- src/js/booking.js
+++ src/js/booking.js
@@ -1,6 +1,30 @@
 const moreInfoBtn = document.querySelector('#pop-up-price button');
 const submitSect = document.querySelector('#submit-book-sect');
+const cancel = document.querySelector('#cancel-submit');
+const title = document.querySelector('#submit-book-title');
+const date = document.querySelector('#submit-book-date');
+const img = document.querySelector('#submit-book-img');
+const description = document.querySelector('#submit-book-description');
+const submitBtn = document.querySelector('#submit-book-submit');
+const main = document.querySelector('main');
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
 
 moreInfoBtn.addEventListener('click', () => {
     submitSect.classList.remove('hide');
+    title.textContent = sessionStorage.getItem('title');
+    date.textContent = sessionStorage.getItem('date');
+    img.src = sessionStorage.getItem('imageSrc');
+    description.textContent = sessionStorage.getItem('description');
+    submitBtn.value = `Zarezerwuj za ${sessionStorage.getItem('price')}`;
+    main.classList.add('blur');
+    header.classList.add('blur');
+    footer.classList.add('blur');
+})
+
+cancel.addEventListener('click', () => {
+    submitSect.classList.add('hide');
+    main.classList.remove('blur');
+    header.classList.remove('blur');
+    footer.classList.remove('blur');
 })
